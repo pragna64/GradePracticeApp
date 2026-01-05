dc724eb969a21e61ab43409b909eb1bfbd0efed4
pragna64 <get2pragna@gmail.com>
2026-01-05 13:49:59 -0500

    code

---
+++ App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Grade Finder")
+grades = ["A","B","C","D","F"]
+chosen = st.selectbox("Choose your grade:", grades)
+if chosen == "A":
+    st.write(f"This student got an {chosen}.")
+else:
+    st.write(f"This student got a {chosen}.")
